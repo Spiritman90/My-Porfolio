242a8c47e9d35a49b14e3ef748b299877a120c94
spiritman90 <phemmyadey@gmail.com>
2022-07-19 13:26:21 +0100

    Added more projects

--- src/data.js
+++ src/data.js
@@ -1,28 +1,17 @@
 export const projects = [
   {
     id: 1,
-    img: "/images/stutern.png",
-    link: "https://spiritman90.github.io/Bi-weekly-challenge2/",
-    description: "Stutern Landing Page, built with HTML and CSS",
+    img: "/images/facts-monster.png",
+    link: "https://flourishing-dasik-334d91.netlify.app/",
+    description:
+      "Facts Monster, an app that provides information about 250 countries of the world built with React, React-redux and SCSS",
   },
   {
     id: 2,
-    img: "/images/shopping cart.png",
-    link: "https://spiritman90.github.io/Shopping-Cart/",
-    description: "A mini shopping cart, built with HTML, CSS and JAVASCRIPT",
-  },
-  {
-    id: 3,
-    img: "/images/edge.png",
-    link: "https://spiritman90.github.io/multi-section-landing-page/",
+    img: "/images/galleryOne.png",
+    link: "https://galleryone.vercel.app/",
     description:
-      "A  multi-section landing page with dynamically-created navigation and a scroll-into-view functionality. Powered by HTML, CSS and JavaScript",
-  },
-  {
-    id: 4,
-    img: "/images/counter.png",
-    link: "https://admiring-euclid-5c169f.netlify.app/",
-    description: "A maiden React Application: Simple Counter App",
+      "GalleryOne is a web app that brings buyers and sellers together. Only users that have signed up or registered on the platform are allowed to bid, buy or sell. User authentication is put in place to register users before onboarding them on the platform. Built with React, SCSS and Redux Toolkit",
   },
   {
     id: 5,
@@ -32,8 +21,22 @@ export const projects = [
       "Traveler is an app that receives the potential destination of a user and then provides the weather update, travel news and side attractions of the location. Technologies used are React, React-Redux, SCSS, and multiple APIs. ",
   },
   {
+    id: 4,
+    img: "/images/counter.png",
+    link: "https://admiring-euclid-5c169f.netlify.app/",
+    description: "A maiden React Application: Simple Counter App",
+  },
+  {
+    id: 3,
+    img: "/images/edge.png",
+    link: "https://spiritman90.github.io/multi-section-landing-page/",
+    description:
+      "A  multi-section landing page with dynamically-created navigation and a scroll-into-view functionality. Powered by HTML, CSS and JavaScript",
+  },
+  {
     id: 6,
-    img: "",
-    link: "",
+    img: "/images/shopping cart.png",
+    link: "https://spiritman90.github.io/Shopping-Cart/",
+    description: "A mini shopping cart, built with HTML, CSS and JAVASCRIPT",
   },
 ];
