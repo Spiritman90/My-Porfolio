26fb39f1454798504fc85ce09f833fd5bd2af497
spiritman90 <phemmyadey@gmail.com>
2023-08-18 01:52:19 +0100

    Updated projects

--- src/data.js
+++ src/data.js
@@ -15,34 +15,48 @@ export const projects = [
   },
   {
     id: 3,
+    img: "/images/fast-pizza2.png",
+    link: "https://fast-pizza-ochre.vercel.app/",
+    description:
+      "Fast Pizza is a Pizza ordering App. Built with Vite + React and TailwindCSS",
+  },
+  {
+    id: 4,
+    img: "/images/travel-list2.png",
+    link: "https://travel-list-teal.vercel.app/",
+    description:
+      "A Travel List App that helps user to pack  items for a trip. Built with React and CSS.",
+  },
+  {
+    id: 5,
     img: "/images/traveler.png",
     link: "https://fervent-montalcini-58eadd.netlify.app/",
     description:
       "Traveler is an app that receives the potential destination of a user and then provides the weather update, travel news and side attractions of the location. Technologies used are React, React-Redux, SCSS, and multiple APIs. ",
   },
   {
-    id: 4,
+    id: 6,
     img: "/images/task-manager.png",
     link: "https://astounding-gingersnap-066e9d.netlify.app/",
     description:
       "A mini task management App, created with React, TypeScript and CSS",
   },
   {
-    id: 5,
+    id: 7,
     img: "/images/advise-generator.png",
     link: "https://advice-generator-zl7r.vercel.app/",
     description:
       "An advise generating App. Techologies used are React, RTK QUery and TailwindCSS",
   },
   {
-    id: 6,
+    id: 8,
     img: "/images/edge.png",
     link: "https://spiritman90.github.io/multi-section-landing-page/",
     description:
       "A  multi-section landing page with dynamically-created navigation and a scroll-into-view functionality. Powered by HTML, CSS and JavaScript",
   },
   {
-    id: 6,
+    id: 9,
     img: "/images/shopping cart.png",
     link: "https://spiritman90.github.io/Shopping-Cart/",
     description: "A mini shopping cart, built with HTML, CSS and JAVASCRIPT",
